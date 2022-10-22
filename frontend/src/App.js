7f9573ee0d2315b3caf7364d8d5c3e33f38e255e
DeveloperFG <firatgoktepe1986@gmail.com>
2022-10-22 18:42:17 +0300

    feat: react router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import {useState, useEffect} from 'react';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddProject from './components/AddProject'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <div className="App">
-      <AddProject />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="addproject" element={<AddProject />} />
+      </Routes>
     </div>
   );  
 }
