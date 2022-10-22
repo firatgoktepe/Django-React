dab94155886a82e11d06c45e70ed586060482655
DeveloperFG <firatgoktepe1986@gmail.com>
2022-10-22 18:40:51 +0300

    feat: db updated

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/andersson/Desktop/tutorial/env"
+set -gx VIRTUAL_ENV "/Users/andersson/Desktop/Django-React-tutorial copy/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
